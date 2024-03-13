8c0389df80c1074a4dc0bc5d2351b1734a251451
satyaprakash2002 <sarangisatya2002@gmail.com>
2024-03-14 00:13:29 +0530

    satya

---
+++ chocolexa/src/components/inedx.js
@@ -0,0 +1,14 @@
+import cake1 from "../assets/cake1.jpg";
+import cake2 from "../assets/black forest.jpg";
+import cake3 from "../assets/vanilla.jpg";
+export const car = [
+  {
+    item: "../assets/vanilla.jpg",
+  },
+  {
+    item: "../assets/black forest.jpg",
+  },
+  {
+    item: "../assets/cake1.jpg",
+  },
+];
